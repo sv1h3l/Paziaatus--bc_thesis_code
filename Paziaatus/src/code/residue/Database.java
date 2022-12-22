99f01dd3d0cb10b9b91d2df908b2a93c9d6583ec
HIGHSENBERG <HIGHSENBERG@HIGHSENBERG_HP>
2022-12-22 20:57:42 +0100

    verze: 1.0

---
+++ Paziaatus/src/code/residue/Database.java
@@ -0,0 +1,16 @@
+package residue;
+
+public class Database
+{
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException
+    {
+		String jdbcURL = "jdbc:h2:";
+		String username = "pda";
+		String password = "paz";
+		
+		try
+		{
+			
+		}
+    }
+}
